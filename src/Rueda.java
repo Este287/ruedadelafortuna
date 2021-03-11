90829f120a20ee361bc7d5abfb7b25c498f85944
unknown <estefaniaroche123@gmail.com>
2021-03-11 00:03:25 -0600

    se creo un juego

---
+++ src/Rueda.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Rueda {
+    public static void main(String[] args) {
+        System.out.println("Rueda de la Fortuna");
+        int Nombre, Ganador;
+        System.out.println("Favor Ingresar los  Nombres");
+        int[] i = new int[4];
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Amor,Paciencia,Soledad,Tristeza");
+        Random entrada = new Random();
+        Nombre = entrada.nextInt();
+
+        System.out.println("El Ganador fue");
+        System.out.println("Soledad,Tristeza");
+        Ganador = entrada.nextInt();
+
+        System.out.print("Los primeros 2 nombres son el ganador y los 2 ultimos  no son");
+        System.out.println("Inge hise todo mi esfuerzo tratare de dar lo mejor");
+
+
+     }
+       }
+
+
+
+
